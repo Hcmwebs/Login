012f32d58eb71d6c5663d07d629a167f08df9576
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-03-18 16:13:53 +1300

    added the formgroup component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,12 +1,10 @@
 import Formgroup from './Formgroup';
 import Button from './Button';
 
-
 const Form = () => {
   return (
     <div className='formContainer'>
       <form action='' method='post'>
-        <Formgroup />
         <Formgroup />
         <Button />
       </form>
--- src/js/components/Formgroup.js
+++ src/js/components/Formgroup.js
@@ -1,10 +1,18 @@
 const Formgroup = () => {
   const name = 'Fullname';
+  const email = 'Email';
   return (
-    <div className='formgroup'>
-      <label htmlFor={name}>Fullname:</label> <br />
-      <input type='text' name={name} id={name} placeholder={name} /> <br />
-    </div>
+    <>
+      <div className='formgroup'>
+        <label htmlFor={name}>{name}</label> <br />
+        <input type='text' name={name} id={name} placeholder={name} /> <br />
+      </div>
+
+      <div className='formgroup'>
+        <label htmlFor={email}>{email}</label> <br />
+        <input type='text' name={email} id={email} placeholder={email} /> <br />
+      </div>
+    </>
   );
 };
 
